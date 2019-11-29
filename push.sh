f82e549777b2e35dc63f20dc5a32413e9ec82f0a
tobicarvalho <tobi.carvalho@gmail.com>
2019-11-29 13:41:18 -0500

    v=0.25

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ -z "$1" ]]
+then
+    echo "ecrit un commentaire mon esti"
+    exit 1
+fi
+
+git add packages/lane_pp/scripts/lane_pp_node.py
+git commit -m $1
+git push
+dts devel build --push -u tobic
+
+docker -H quackmobile.local pull tobic/pp -a
+dts duckiebot demo --demo_name lane_pp --package_name lane_pp --duckiebot_name quackmobile --image tobic/pp:v1-arm32v7
+docker -H quackmobile.local logs -f demo_lane_pp
